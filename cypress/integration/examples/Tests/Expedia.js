8ff9428639b3af63ad573bacc69f8892733ca90e
Girish Nair <nairgirish100@gmail.com>
2020-06-21 15:17:56 +0530

    updated expedia file

--- cypress/integration/examples/Tests/Expedia.js
+++ cypress/integration/examples/Tests/Expedia.js
@@ -3,7 +3,7 @@ describe('Expedia Tests', function() {
 
 it('Launch URL', function() {
 
-    cy.visit('https://www.expedia.com/', {failOnStatusCode: false})
+    cy.visit('https://www.expedia.com/', {failOnStatusCode: false});
 })
 
 
